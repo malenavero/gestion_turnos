256b6b659d38ae46f16ef82c3728323ab8a0e16b
malenavero <malena@codigolibre.coop.ar>
2024-09-06 20:27:35 -0300

    set admin como home

--- gestion_turnos/urls.py
+++ gestion_turnos/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Aca seteo que el admin se abra por default, si hacemos una vista hay que sacarlo
+    path('', lambda request: redirect('admin/', permanent=True)),
 ]
