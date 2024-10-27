52afdae421c70c9e8039701dbe49bdeddb8f43e2
malenavero <malena@codigolibre.coop.ar>
2024-10-27 20:37:44 -0300

    fix icono + fix middleware

--- turnos/middleware.py
+++ turnos/middleware.py
@@ -4,9 +4,10 @@ class PreviousUrlMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Guardar la URL anterior en la sesión
-        if 'previous_url' not in request.session:
-            request.session['previous_url'] = request.META.get('HTTP_REFERER')
+        # Guardar la URL anterior en la sesión solo si HTTP_REFERER está presente
+        if 'HTTP_REFERER' in request.META:
+            request.session['previous_url'] = request.META['HTTP_REFERER']
         
         response = self.get_response(request)
         return response
+
