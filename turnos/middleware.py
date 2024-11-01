74e09d58ac74610593e97fb8411e1254c9b4dd76
malenavero <malena@codigolibre.coop.ar>
2024-11-01 16:41:28 -0300

    fix middleware back button

--- turnos/middleware.py
+++ turnos/middleware.py
@@ -1,13 +1,31 @@
-# middleware.py
+from django.urls import reverse
+
+
 class PreviousUrlMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Guardar la URL anterior en la sesión solo si HTTP_REFERER está presente
-        if 'HTTP_REFERER' in request.META:
-            request.session['previous_url'] = request.META['HTTP_REFERER']
-        
+        # Llamar a la respuesta sin procesar la vista (esto permite usar process_view más adelante)
         response = self.get_response(request)
         return response
 
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        # Obtener el nombre de la vista actual usando `view_func` en `process_view`
+        current_view = view_func.__name__
+
+               # Definir la URL anterior según las reglas específicas, usando `reverse`
+        if current_view in ["dar_presente", "turnero"]:
+            request.session['previous_url'] = reverse('recepcion')
+        
+        elif current_view in ["turnero_bloquear", "turnero_cancelar", "turnero_reservar"]:
+            request.session['previous_url'] = reverse('turnero')
+
+        elif current_view in ["lista_pacientes", "lista_especialidad", "lista_medicos"]:
+            request.session['previous_url'] = reverse('fabrica')
+        
+        elif current_view in ["recepcion", "atencion", "liquidacion_honorarios", "fabrica"]:
+            request.session['previous_url'] = reverse('main')
+        # Si no coincide con ninguna vista específica, no modifica `previous_url`
+
+        return None
