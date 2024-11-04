30c440a6e3db8a0d98d0d074847a16ecd0e67ef3
malenavero <malena@codigolibre.coop.ar>
2024-11-03 22:12:02 -0300

    deshabilitar especialidad si se selecciona el filtro de medico y viseversa en resevar turnos

--- turnos/static/js/ui_funciones.js
+++ turnos/static/js/ui_funciones.js
@@ -9,3 +9,27 @@ function closeErrorMessage() {
 function closeAlert(idAlert) {
     document.getElementById(idAlert).style.display = 'none';
 }
+
+function toggleEspecialidad() {
+    const medicoSelect = document.getElementById('medico');
+    const especialidadSelect = document.getElementById('especialidad');
+
+    // Si hay un médico seleccionado, deshabilitar especialidad
+    if (medicoSelect.value) {
+        especialidadSelect.disabled = true;
+    } else {
+        especialidadSelect.disabled = false;
+    }
+}
+
+function toggleMedico() {
+    const especialidadSelect = document.getElementById('especialidad');
+    const medicoSelect = document.getElementById('medico');
+
+    // Si hay una especialidad seleccionada, deshabilitar médico
+    if (especialidadSelect.value) {
+        medicoSelect.disabled = true;
+    } else {
+        medicoSelect.disabled = false; // Habilitar si no hay especialidad seleccionada
+    }
+}
